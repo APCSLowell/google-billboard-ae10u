218f6776ce9e32c2b20fe31e53cbf00948cd7e34
StephenMiner <64863697+StephenMiner@users.noreply.github.com>
2024-01-16 17:33:55 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,7 +1,13 @@
 
 
 public class GoogleBillboard {
-	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
+
+	//public static void main(String[] args) is required in a java program!
+	//Note that you can only reference/call other static methods/variables inside of a static method
+	public static void main(String[] args){
+		System.out.println("Replace this String with your answer");
+	}
 public void setup()  
 {            
   String digits = e.substring(2,12);
